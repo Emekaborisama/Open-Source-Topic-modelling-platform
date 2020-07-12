e1d165c5998548fd3a142490d4102c5418118fc2
Emeka boris ama <borisphilosophy@gmail.com>
2020-07-12 01:02:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ cv = CountVectorizer()
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 import pickle
 
-
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #Title
 
